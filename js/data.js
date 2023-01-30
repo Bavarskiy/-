0f130d7f0706282cefd2a8c3f4294128102e2e31
Саша Кибасов <sashaserp@yandex.ru>
2023-01-30 11:50:58 +0300

    дз 5.1

--- js/data.js
+++ js/data.js
@@ -1,9 +1,9 @@
 import { getRandomNumber, getRandomElementArr } from './util.js';
 // Раздел 4
 
-const PHOTO_QUANTITY = 25;
+const PAGE_MINIATURES_QUANTITY = 25;
 
-const NAMES = [
+const PAGE_MINIATURES_NAMES = [
   'Саша',
   'Вася',
   'Ника',
@@ -13,7 +13,7 @@ const NAMES = [
   'Коля',
 ];
 
-const photoDescriptions = [
+const pageMiniaturesDescriptions = [
   'Любимое фото',
   'Яркое воспоминание',
   'Хочу сюда вернуться',
@@ -23,22 +23,22 @@ const photoDescriptions = [
   'Райское место',
 ];
 
-const PhotoLikes = {
+const pageMiniaturesLikes = {
   MIN: 15,
   MAX: 200,
 };
 
-const PhotoCommentsQuantity = {
+const pageMiniaturesCommentsQuantity = {
   MIN: 1,
   MAX: 5,
 };
 
-const AvatarNumber = {
+const pageMiniaturesAvatarRandomNumber = {
   MIN: 1,
   MAX: 6,
 };
 
-const photoMessages = [
+const pageMiniaturesRandomUserMessages = [
   'Всё отлично!',
   'В целом всё неплохо.Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.В конце концов это просто непрофессионально.',
@@ -47,37 +47,37 @@ const photoMessages = [
   'Лица у людей на фотке перекошены, как будто их избивают.Как можно было поймать такой неудачный момент ?!',
 ];
 
-const photoContent = [];
+const pageMiniatures = [];
 
-const createPhotoContent = () => {
+const createPageMiniatures = () => {
 
-  for (let i = 0; i < PHOTO_QUANTITY; i++) {
-    photoContent.push({
+  for (let i = 0; i < PAGE_MINIATURES_QUANTITY; i++) {
+    pageMiniatures.push({
       id: i,
       url: 'photos/' + (i + 1) + '.jpg',
-      description: getRandomElementArr(photoDescriptions),
-      likes: getRandomNumber(PhotoLikes.MIN, PhotoLikes.MAX),
-      comments: createComments(),
+      description: getRandomElementArr(pageMiniaturesDescriptions),
+      likes: getRandomNumber(pageMiniaturesLikes.MIN, pageMiniaturesLikes.MAX),
+      comments: createPageMiniaturesComments(),
     });
   }
 };
 
-const createComments = () => {
+const createPageMiniaturesComments = () => {
   const comments = [];
 
-  for (let i = 0; i < getRandomNumber(PhotoCommentsQuantity.MIN, PhotoCommentsQuantity.MAX); i++) {
+  for (let i = 0; i < getRandomNumber(pageMiniaturesCommentsQuantity.MIN, pageMiniaturesCommentsQuantity.MAX); i++) {
     comments.push({
       // Ниже магические цифры. Я не стал делать отдельную const для этого id,
       // потому что есть id комментария, и есть id комментатора. В нейминге можно запутаться
       id: getRandomNumber(1, 999),
-      avatar: 'img/avatar-' + getRandomNumber(AvatarNumber.MIN, AvatarNumber.MAX) + '.svg',
-      message: getRandomElementArr(photoMessages),
-      name: getRandomElementArr(NAMES),
+      avatar: 'img/avatar-' + getRandomNumber(pageMiniaturesAvatarRandomNumber.MIN, pageMiniaturesAvatarRandomNumber.MAX) + '.svg',
+      message: getRandomElementArr(pageMiniaturesRandomUserMessages),
+      name: getRandomElementArr(PAGE_MINIATURES_NAMES),
     });
   }
   return comments;
 };
 
-createPhotoContent();
+createPageMiniatures();
 
-export { photoContent, createPhotoContent };
+export { pageMiniatures, createPageMiniatures };
